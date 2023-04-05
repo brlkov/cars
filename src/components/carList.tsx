672b837155624e6a456f3fd89c769cbf3f576a9a
brlkov <sergeyburlakovspb@gmail.com>
2023-04-05 22:29:04 +0300

    1

---
+++ src/components/carList.tsx
@@ -0,0 +1,34 @@
+import React, {FC, useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {addCars, state} from "../store/reducer";
+import CarItem from "./carItem";
+import "../styles/carList.scss"
+import {Api} from "../api";
+
+const CarList: FC = () => {
+
+    const dispatch = useDispatch()
+    const cars = useSelector((state: state)  => state.cars)
+    const sort = useSelector((state: state)  => state.sort)
+
+    useEffect(() => {
+        dispatch(getCars())
+    }, [])
+
+    const getCars: any = () => {
+        return (dispatch: any) => {
+            Api.getAllCars()
+                .then(response => dispatch(addCars(response.data)))
+        }
+    }
+
+    return (
+        <div className="car-list">
+            {cars.map(car =>
+                <CarItem car={car} key={car.id}/>
+            )}
+        </div>
+    );
+};
+
+export default CarList;
